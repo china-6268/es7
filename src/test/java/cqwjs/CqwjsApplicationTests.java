4dd88209ec1b70f3df5e52e199565d79edac5dc5
刘增玉 <liuzy@jr13.cn>
2021-06-09 09:59:31 +0800

    pom书写格式优化1

--- src/test/java/cqwjs/CqwjsApplicationTests.java
+++ src/test/java/cqwjs/CqwjsApplicationTests.java
@@ -1,9 +1,15 @@
 package cqwjs;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import javax.ws.rs.core.Application;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes= Application.class)
 class CqwjsApplicationTests {
 
     @Test
